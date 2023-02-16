3ee084cf2042c5d6f73bd5b3283728e28fe69295
Moritz Hohmann <moritz.hohmann@hbbk-office.de>
2023-02-16 11:06:55 +0100

    Fertig

--- SuperBrain 2.0/MainWindow.xaml.cs
+++ SuperBrain 2.0/MainWindow.xaml.cs
@@ -30,47 +30,21 @@ namespace SuperBrain_2._0
             InitializeComponent();
         }
 
-        List<int>[] randomNumbers = new List<int>[2];
+        List<int>[] randomNumbers = new List<int>[1];
 
-        List<int>[] PlayerNumbers = new List<int>[2];
+        List<int>[] PlayerNumbers = new List<int>[1];
 
         List<TextBox> textBoxes = new List<TextBox>();
 
         async void Main()
         {
-            int f = 10;
-            if (f == 4)
-            {
-                StackPanelTextBoxen.Width = 194;
-                StackPanelTextBoxen.Margin = new Thickness(403, 0, 0, 0);
-            }
-            else if (f == 6)
-            {
-                StackPanelTextBoxen.Width = 294;
-                StackPanelTextBoxen.Margin = new Thickness(353, 0, 0, 0);
-            }
-            else if (f == 8) 
-            {
-                StackPanelTextBoxen.Width = 394;
-                StackPanelTextBoxen.Margin = new Thickness(303, 0, 0, 0);
-            }
-            else if (f == 10)
-            {
-                StackPanelTextBoxen.Width = 494;
-                StackPanelTextBoxen.Margin = new Thickness(253, 0, 0, 0);
-            }
+            Check();
 
             Random random = new();
 
-            textBoxes.Add(Box1);
-            textBoxes.Add(Box2);
-            textBoxes.Add(Box3);
-            textBoxes.Add(Box4);
-            textBoxes.Add(Box5);
-            textBoxes.Add(Box6);
 
-            RandomNumbers(6);
-
+            int speed = Convert.ToInt32(Convert.ToString(SliderSpeed.Value) + "000");
+           
             int count = 10;
             for (int i = 0; i < randomNumbers.Length; i++)
             {
@@ -100,7 +74,7 @@ namespace SuperBrain_2._0
 
 
                 Box1.Focus();
-                await Task.Delay(10000);
+                await Task.Delay(speed);
 
                 for (int i2 = 0; i2 < textBoxes.Count; i2++)
                 {
@@ -112,7 +86,7 @@ namespace SuperBrain_2._0
 
             TextBlockCount.Visibility= Visibility.Hidden;
             StackPanelTextBoxen.Visibility = Visibility.Hidden;
-            ListBox.Visibility= Visibility.Visible;
+            ListWin.Visibility= Visibility.Visible;
             ListBox1.Visibility= Visibility.Visible;
 
             for (int i = 0; i < randomNumbers.Length; i++)
@@ -127,19 +101,110 @@ namespace SuperBrain_2._0
                     
                 }
 
-                ListBox1.Items.Add(Convert.ToString(randomNumbers[i][0]) + Convert.ToString(randomNumbers[i][1]) + Convert.ToString(randomNumbers[i][2]) + Convert.ToString(randomNumbers[i][3]) + Convert.ToString(randomNumbers[i][4]) + Convert.ToString(randomNumbers[i][5]) + " ---- " + PlayerNumbers[i][0] + PlayerNumbers[i][1] + PlayerNumbers[i][2] + PlayerNumbers[i][3] + PlayerNumbers[i][4] + PlayerNumbers[i][5]);
+                if (textBoxes.Count == 4)
+                {
+                    ListBox1.Items.Add(Convert.ToString(randomNumbers[i][0]) + Convert.ToString(randomNumbers[i][1]) + 
+                        Convert.ToString(randomNumbers[i][2]) + Convert.ToString(randomNumbers[i][3]) + 
+                        " ---- " + PlayerNumbers[i][0] + PlayerNumbers[i][1] + PlayerNumbers[i][2] + 
+                        PlayerNumbers[i][3]);
+                    ListWin.Margin = new Thickness(190, 71, 17, 0);
+                }
+                else if (textBoxes.Count == 6)
+                {
+                    ListBox1.Items.Add(Convert.ToString(randomNumbers[i][0]) + Convert.ToString(randomNumbers[i][1]) +
+                        Convert.ToString(randomNumbers[i][2]) + Convert.ToString(randomNumbers[i][3]) + Convert.ToString(randomNumbers[i][4]) + Convert.ToString(randomNumbers[i][5]) +
+                        " ---- " + PlayerNumbers[i][0] + PlayerNumbers[i][1] + PlayerNumbers[i][2] +
+                        PlayerNumbers[i][3] + PlayerNumbers[i][4] + PlayerNumbers[i][5]);
+                    ListWin.Margin = new Thickness(250, 71, 17, 0);
+                }
+                else if (textBoxes.Count == 8)
+                {
+                    ListBox1.Items.Add(Convert.ToString(randomNumbers[i][0]) + Convert.ToString(randomNumbers[i][1]) +
+                        Convert.ToString(randomNumbers[i][2]) + Convert.ToString(randomNumbers[i][3]) + Convert.ToString(randomNumbers[i][4]) + Convert.ToString(randomNumbers[i][5]) + 
+                        Convert.ToString(randomNumbers[i][6]) + Convert.ToString(randomNumbers[i][7]) +
+                        " ---- " + PlayerNumbers[i][0] + PlayerNumbers[i][1] + PlayerNumbers[i][2] +
+                        PlayerNumbers[i][3] + PlayerNumbers[i][4] + PlayerNumbers[i][5] + PlayerNumbers[i][6] + PlayerNumbers[i][7]);
+                    ListWin.Margin = new Thickness(305, 71, 17, 0);
+                }
+                else if (textBoxes.Count == 10)
+                {
+                    ListBox1.Items.Add(Convert.ToString(randomNumbers[i][0]) + Convert.ToString(randomNumbers[i][1]) +
+                        Convert.ToString(randomNumbers[i][2]) + Convert.ToString(randomNumbers[i][3]) + Convert.ToString(randomNumbers[i][4]) + Convert.ToString(randomNumbers[i][5]) +
+                        Convert.ToString(randomNumbers[i][6]) + Convert.ToString(randomNumbers[i][7]) + Convert.ToString(randomNumbers[i][8]) + Convert.ToString(randomNumbers[i][9]) +
+                        " ---- " + PlayerNumbers[i][0] + PlayerNumbers[i][1] + PlayerNumbers[i][2] +
+                        PlayerNumbers[i][3] + PlayerNumbers[i][4] + PlayerNumbers[i][5] + PlayerNumbers[i][6] + PlayerNumbers[i][7] + PlayerNumbers[i][8] + PlayerNumbers[i][9]);
+                    ListWin.Margin = new Thickness(380, 71, 17, 0);
+                }
+                
 
                 if (KeinFehler) {
                     int temp = i + 1;
-                    ListBox.Items.Add(temp + " Versuch"  + " Win");
+                    ListWin.Items.Add(temp + " Versuch"  + " Win");
                 }
                 else {
                     int temp = i + 1;
-                    ListBox.Items.Add(temp + " Versuch" + " Falsch");
+                    ListWin.Items.Add(temp + " Versuch" + " Falsch");
                 }
             }
         }
         
+
+        void Check()
+        {
+            if (SliderBoxes.Value == 1)
+            {
+                StackPanelTextBoxen.Width = 194;
+                StackPanelTextBoxen.Margin = new Thickness(403, 0, 0, 0);
+                RandomNumbers(4);
+                textBoxes.Add(Box1);
+                textBoxes.Add(Box2);
+                textBoxes.Add(Box3);
+                textBoxes.Add(Box4);
+            }
+            else if (SliderBoxes.Value == 2)
+            {
+                StackPanelTextBoxen.Width = 294;
+                StackPanelTextBoxen.Margin = new Thickness(353, 0, 0, 0);
+                RandomNumbers(6);
+                textBoxes.Add(Box1);
+                textBoxes.Add(Box2);
+                textBoxes.Add(Box3);
+                textBoxes.Add(Box4);
+                textBoxes.Add(Box5);
+                textBoxes.Add(Box6);
+            }
+            else if (SliderBoxes.Value == 3)
+            {
+                StackPanelTextBoxen.Width = 394;
+                StackPanelTextBoxen.Margin = new Thickness(303, 0, 0, 0);
+                RandomNumbers(8);
+                textBoxes.Add(Box1);
+                textBoxes.Add(Box2);
+                textBoxes.Add(Box3);
+                textBoxes.Add(Box4);
+                textBoxes.Add(Box5);
+                textBoxes.Add(Box6);
+                textBoxes.Add(Box7);
+                textBoxes.Add(Box8);
+            }
+            else if (SliderBoxes.Value == 4)
+            {
+                StackPanelTextBoxen.Width = 494;
+                StackPanelTextBoxen.Margin = new Thickness(253, 0, 0, 0);
+                RandomNumbers(10);
+                textBoxes.Add(Box1);
+                textBoxes.Add(Box2);
+                textBoxes.Add(Box3);
+                textBoxes.Add(Box4);
+                textBoxes.Add(Box5);
+                textBoxes.Add(Box6);
+                textBoxes.Add(Box7);
+                textBoxes.Add(Box8);
+                textBoxes.Add(Box9);
+                textBoxes.Add(Box10);
+            }
+        }
+
         void RandomNumbers(int howmany)
         {
             Random random = new();
@@ -159,6 +224,10 @@ namespace SuperBrain_2._0
 
             Button1.Visibility = Visibility.Hidden;
             Welcom.Visibility = Visibility.Hidden;
+            Label1.Visibility = Visibility.Hidden;
+            Label2.Visibility = Visibility.Hidden;
+            SliderBoxes.Visibility = Visibility.Hidden;
+            SliderSpeed.Visibility = Visibility.Hidden;
             Main();
         }
 
@@ -198,7 +267,14 @@ namespace SuperBrain_2._0
         {
             if (Box4.Text.Length >= 1)
             {
-                Box5.Focus();
+                if (textBoxes.Count == 4)
+                {
+                    lost.Focus();
+                }
+                else
+                {
+                    Box5.Focus();
+                }
                 Box4.Text = Box4.Text.Substring(0, 1);
                 Box4.SelectionStart = 1;
             }
@@ -216,12 +292,65 @@ namespace SuperBrain_2._0
 
         private void Box6_TextChanged(object sender, TextChangedEventArgs e)
         {
-            StackPanelTextBoxen.Focus();
+            //StackPanelTextBoxen.Focus();
             if (Box6.Text.Length >= 1)
             {
-                lost.Focus();
+                if (textBoxes.Count == 6)
+                {
+                    lost.Focus();
+                }
+                else
+                {
+                    Box7.Focus();
+                }
                 Box6.Text = Box6.Text.Substring(0, 1);
                 Box6.SelectionStart = 1;
+            }
+        }
+        private void Box7_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (Box7.Text.Length >= 1)
+            {
+                Box8.Focus();
+                Box7.Text = Box7.Text.Substring(0, 1);
+                Box7.SelectionStart = 1;
+            }
+        }
+
+        private void Box8_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (Box8.Text.Length >= 1)
+            {
+                if (textBoxes.Count == 8)
+                {
+                    lost.Focus();
+                }
+                else
+                {
+                    Box9.Focus();
+                }
+                Box8.Text = Box8.Text.Substring(0, 1);
+                Box8.SelectionStart = 1;
+            }
+        }
+
+        private void Box9_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (Box9.Text.Length >= 1)
+            {
+                Box10.Focus();
+                Box9.Text = Box9.Text.Substring(0, 1);
+                Box9.SelectionStart = 1;
+            }
+        }
+
+        private void Box10_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (Box10.Text.Length >= 1)
+            {
+                lost.Focus();
+                Box10.Text = Box10.Text.Substring(0, 1);
+                Box10.SelectionStart = 1;
             }
         }
 
@@ -265,12 +394,59 @@ namespace SuperBrain_2._0
             }
         }
 
-        private void lost_PreviewKeyDown(object sender, KeyEventArgs e)
+        private void Box7_PreviewKeyDown(object sender, KeyEventArgs e)
         {
-            if(e.Key == Key.Back)
+            if (e.Key == Key.Back)
             {
                 Box6.Focus();
             }
         }
+
+        private void Box8_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Back)
+            {
+                Box7.Focus();
+            }
+        }
+
+        private void Box9_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Back)
+            {
+                Box8.Focus();
+            }
+        }
+
+        private void Box10_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Back)
+            {
+                Box9.Focus();
+            }
+        }
+
+        private void lost_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.Back)
+            {
+                if(textBoxes.Count == 4)
+                {
+                    Box4.Focus();
+                }else if (textBoxes.Count == 6)
+                {
+                    Box6.Focus();
+                }else if (textBoxes.Count == 8)
+                {
+                    Box8.Focus();
+                }else if (textBoxes.Count == 10)
+                {
+                    Box10.Focus();
+                }
+                
+            }
+        }
+
+        
     }
 }
